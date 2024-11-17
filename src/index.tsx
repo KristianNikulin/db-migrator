1311351459a60e2dd4a9dbf19084013eb84d02ca
Kristian Nikulin <kristiannikulin@mail.ru>
2024-11-17 19:02:05 +0300

    feat: add global provider for toastify

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import App from "./pages";
+import { GlobalContextProvider } from "./state-providers/global";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
     // <React.StrictMode>
-    <App />
+    <GlobalContextProvider>
+        <App />
+    </GlobalContextProvider>
     // </React.StrictMode>
 );
